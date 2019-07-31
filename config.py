4f6fb35fe9fda00796bcb898b2c1cd446af35efd
abhishekhingne <hingneabhishek@gmail.com>
2019-07-31 15:35:47 +0530

    config added

---
+++ config.py
@@ -0,0 +1,8 @@
+ARTICLE_PATH = "data/article.txt"
+SUMMARY_PATH = "data/summary.txt"
+VALID_ARTICLE_PATH = "data/valid_article.txt"
+VALID_SUMMARY_PATH = "data/valid_summary.txt"
+WORD_DICT_PATH = "output/word_dict.pickle"
+MAX_ARTICLE_LEN = 50
+MAX_SUMMARY_LEN = 15
+GLOVE_EMBEDDING_PATH = "data/glove.6B.300d.txt"
