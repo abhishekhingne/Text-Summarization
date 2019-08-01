fdc3a46ae83bafe166a8150919eb488beef52cda
abhishekhingne <hingneabhishek@gmail.com>
2019-08-01 16:07:14 +0530

    training code added

---
+++ run.py
@@ -0,0 +1,39 @@
+from build_data import BuildData
+from seq2seq import TextSummarization
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_hidden", type=int, default=150, help="Network size.")
+    parser.add_argument("--num_layers", type=int, default=2, help="Network depth.")
+    parser.add_argument("--beam_width", type=int, default=10, help="Beam width for beam search decoder.")
+    parser.add_argument("--embedding_size", type=int, default=300, help="Word embedding size.")
+    parser.add_argument("--learning_rate", type=float, default=1e-3, help="Learning rate.")
+    parser.add_argument("--batch_size", type=int, default=64, help="Batch size.")
+    parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs.")
+    parser.add_argument("--keep_prob", type=float, default=0.8, help="Dropout keep prob.")
+    parser.add_argument("--mode", type=str , default="train", help="Train or Test")
+    parser.add_argument("--with_model", action="store_true", help="Continue from previously saved model")
+
+    args = parser.parse_args()
+    num_hidden = args.num_hidden
+    num_layers = args.num_layers
+    beam_width = args.beam_width
+    embedding_size = args.embedding_size
+    learning_rate = args.learning_rate
+    batch_size = args.batch_size
+    num_epochs = args.num_epochs
+    keep_prob = args.keep_prob
+    mode = args.mode
+    with_model = args.with_model
+    data = BuildData()
+    text_summarization = TextSummarization(num_hidden=num_hidden, num_layers=num_layers, beam_width=beam_width,
+                                           embedding_size=embedding_size, learning_rate=learning_rate,
+                                           batch_size=batch_size, num_epochs=num_epochs, keep_prob=keep_prob,
+                                           with_model=False)
+    if mode == "train":
+        data.load_data()
+        data.build_dict()
+        train_x, train_y = data.build_dataset
+        text_summarization.start_training(train_x=train_x, train_y=train_y, word_dict=data.word_dict,
+                                          reversed_word_dict=data.reversed_word_dict)
---
+++ seq2seq.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+from model import Model
+from build_data import *
+import time
+from config import *
+
+
+class TextSummarization:
+
+    def __init__(self, num_hidden=150, num_layers=2, beam_width=10, embedding_size=300, learning_rate=1e-3,
+                 batch_size=64, num_epochs=100, keep_prob=0.8, with_model=False):
+        self.num_hidden = num_hidden
+        self.num_layers = num_layers
+        self.beam_width = beam_width
+        self.embedding_size = embedding_size
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+        self.keep_prob = keep_prob
+        self.with_model = with_model
+        self.data = BuildData()
+        self.start_time = time.time()
+
+    def start_training(self, train_x, train_y, word_dict, reversed_word_dict):
+        tf.reset_default_graph()
+
+        with tf.Session() as sess:
+            model = Model(reversed_word_dict, MAX_ARTICLE_LEN, MAX_SUMMARY_LEN, self.embedding_size, self.num_hidden,
+                          self.num_layers, self.learning_rate, self.beam_width, self.keep_prob)
+            sess.run(tf.global_variables_initializer())
+            saver = tf.train.Saver(tf.global_variables())
+
+            batches = self.data.batch_iter(train_x, train_y, self.batch_size, self.num_epochs)
+            num_batches_per_epoch = (len(train_x) - 1) // self.batch_size + 1
+
+            print("\nTraining Started.")
+            print("Number of batches per epoch :", num_batches_per_epoch)
+            for batch_x, batch_y in batches:
+                batch_x_len = list(map(lambda x: len([y for y in x if y != 0]), batch_x))
+                batch_decoder_input = list(map(lambda x: [word_dict["<s>"]] + list(x), batch_y))
+                batch_decoder_len = list(map(lambda x: len([y for y in x if y != 0]), batch_decoder_input))
+                batch_decoder_output = list(map(lambda x: list(x) + [word_dict["</s>"]], batch_y))
+
+                batch_decoder_input = list(
+                    map(lambda d: d + (MAX_SUMMARY_LEN - len(d)) * [word_dict["<padding>"]], batch_decoder_input))
+                batch_decoder_output = list(
+                    map(lambda d: d + (MAX_SUMMARY_LEN - len(d)) * [word_dict["<padding>"]], batch_decoder_output))
+
+                train_feed_dict = {
+                    model.batch_size: len(batch_x),
+                    model.X: batch_x,
+                    model.X_len: batch_x_len,
+                    model.decoder_input: batch_decoder_input,
+                    model.decoder_len: batch_decoder_len,
+                    model.decoder_target: batch_decoder_output
+                }
+
+                _, step, loss = sess.run([model.update, model.global_step, model.loss], feed_dict=train_feed_dict)
+
+                if step % 1000 == 0:
+                    print("step {0}: loss = {1}".format(step, loss))
+
+                if step % num_batches_per_epoch == 0:
+                    hours, rem = divmod(time.time() - self.start_time, 3600)
+                    minutes, seconds = divmod(rem, 60)
+                    saver.save(sess, MODEL_PATH + "model.ckpt", global_step=step)
+                    print(" Epoch {0}: Model is saved.".format(step // num_batches_per_epoch),
+                          "Elapsed: {:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds), "\n")
