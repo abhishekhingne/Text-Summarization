3347eb27523c0d1d9b5b18529253fa1e1df03f2a
abhishekhingne <hingneabhishek@gmail.com>
2019-08-01 18:03:33 +0530

    training code added

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=64, help="Batch size.")
     parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs.")
     parser.add_argument("--keep_prob", type=float, default=0.8, help="Dropout keep prob.")
-    parser.add_argument("--mode", type=str , default="train", help="Train or Test")
+    parser.add_argument("--mode", type=str, default="train", help="Train or Test")
     parser.add_argument("--with_model", action="store_true", help="Continue from previously saved model")
 
     args = parser.parse_args()
