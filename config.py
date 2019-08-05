d3d70ac9b8c9739c86d827fc73170be479fe8d47
abhishekhingne <hingneabhishek@gmail.com>
2019-08-05 17:09:43 +0530

    output result path for validation set added

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ SUMMARY_PATH = "data/summary.txt"
 VALID_ARTICLE_PATH = "data/valid_article.txt"
 VALID_SUMMARY_PATH = "data/valid_summary.txt"
 WORD_DICT_PATH = "output/word_dict.pickle"
+RESULT_PATH = "output/result.txt"
 MAX_ARTICLE_LEN = 50
 MAX_SUMMARY_LEN = 15
 GLOVE_EMBEDDING_PATH = "data/glove.6B.300d.txt"
